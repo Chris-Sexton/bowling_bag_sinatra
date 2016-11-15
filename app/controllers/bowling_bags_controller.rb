4fc63003e180df27b9496e97fe1cf0df81a2b20f
Chris-Sexton <champlain2013@gmail.com>
2016-11-15 10:12:11 -0500

    Added more code to the bowling bags controller.

--- app/controllers/bowling_bags_controller.rb
+++ app/controllers/bowling_bags_controller.rb
@@ -1,4 +1,4 @@
-class GolfBagsController < ApplicationController 
+class BowlingBagsController < ApplicationController 
 
   get "/bags" do
     redirect_if_not_logged_in
@@ -19,4 +19,29 @@ class GolfBagsController < ApplicationController
     erb :'bowling_bags/edit'
   end
 
+  post "/bags/:id" do
+    redirect_if_not_logged_in
+    @bag = BowlingBag.find(params[:id])
+    unless BowlingBag.valid_params?(params)
+      redirect "/bags/#{@bag.id}/edit?error=invalid bowling bag"
+    end
+    @bag.update(params.select{|b| b =="name" || b =="capacity"})
+    redirect "/bags/#{@bag.id}"
+  end
+
+  get "/bags/:id" do
+    redirect_if_not_logged_in
+    @bag = BowlingBag.find(params[:id])
+    erb :'bowling_bags/show'
+  end
+
+  post "/bags" do
+    redirect_if_not_logged_in
+
+    unless BowlingBag.valid_params?(params)
+      redirect "/bags/new?error=invalid golf bag"
+    end
+    BowlingBag.create(params)
+    redirect "/bags"
+  end
 end
